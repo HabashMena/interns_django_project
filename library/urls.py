acf6c7f4066a44c2f817b5ebb065dfad0f75d01c
leen dweiri <leendweiri80@gmail.com>
2025-04-21 14:43:52 +0300

    ctreat

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('fbv/', views.book_list_fbv, name='book_list_fbv'),
-    path('cbv/', views.BookListCBV.as_view(), name='book_list_cbv'),
-    path('gbv/', views.BookListGBV.as_view(), name='book_list_gbv'),
+    path('functionbasedview/', views.book_list_fbv, name='book_list_fbv'),
+    path('genericbasedview/', views.BookListGBV.as_view(), name='book_list_gbv'),
+    path('classbasedview/', views.BookListCBV.as_view(), name='book_list_cbv'),
+
+    path('signup/fbv/', views.signup_fbv, name='signup_fbv'),
+    path('signup/cbv/', views.BookListCBV.SignupCBV.as_view(), name='signup_cbv'),
+    path('signup/gbv/', views.SignupGBV.as_view(), name='signup_gbv'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,17 +1,49 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render, redirect
 from .models import Book
+
 def book_list_fbv(request):
     books=Book.objects.all()
     return render(request,'library/book_list.html',{'books': books})
+
+def signup_fbv(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'library/signup.html', {'form': form, 'view_type': 'FBV Signup'})
+
 #gbv:
 
 from django.views.generic.list import ListView
 from .models import Book
+
 class BookListGBV(ListView):
-   model = Book
-template_name='library/book_list.html'
-context_object_name='books'
+ model = Book
+ template_name='library/book_list.html'
+ context_object_name = 'books'
+
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+class SignupGBV(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # or wherever you want
+    template_name = 'signup.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['view_type'] = 'GBV Signup'
+        return context
+
 
 
 
@@ -25,5 +57,16 @@ class BookListCBV(View):
     def get(self,request):
       books = Book.objects.all()
       return render(request,'library/book_list.html',{'books':books})
+    class SignupCBV(View):
+     def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'library/signup.html', {'form': form, 'view_type': 'CBV Signup'})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'library/signup.html', {'form': form, 'view_type': 'CBV Signup'})
 
 
